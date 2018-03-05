b21e25143c1cd263c2f5d06b64bc32975c466c0a
leocadiob7576 <leocadiob7576@hstat.org>
2018-03-05 16:58:27 +0000

    work on resturants.rb

--- application_controller.rb
+++ application_controller.rb
@@ -4,7 +4,9 @@ require 'romato'
 
 Bundler.require
 
-require_relative 'models/model.rb'
+require_relative 'models/recipe-search.rb'
+require_relative 'models/restaurants.rb'
+require_relative 'models/reviews.rb'
 
 class ApplicationController < Sinatra::Base
 
