cc8b634251cc51a55a26942cd625e247a4b776d0
davidx4697 <davidx4697@hstat.org>
2018-03-08 17:01:53 +0000

    🍆🍑

--- models/reviews.rb
+++ models/reviews.rb
@@ -2,7 +2,7 @@
 # require 'json'
 # require 'pp'
 
-<<<<<<< HEAD
+
 url = 'https://thereportoftheweek-api.herokuapp.com/reports?category=Energy%20Crisis'
 uri = URI(url)
 response = Net::HTTP.get(uri)
@@ -11,6 +11,3 @@ pp JSON.parse(response)\
 class Review
     
 end
-=======
-
->>>>>>> 5ed0277adf3df5632a51c7ae3c6c2807c4a34cd5
