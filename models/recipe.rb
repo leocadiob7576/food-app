1b481aed9044754f7433d74f31062f96e695c13e
ricardop2476 <ricardop2476@hstat.org>
2018-03-08 18:08:58 +0000

    add gem

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,5 +1,2 @@
-# require 'edamam-ruby'
+# https://github.com/CreoMark/food2fork-ruby
 
-# client = Edamam::Client.new(app_id: "b5ae6e16", app_key: "99df03635880bb97c81ddf53c476f91a")
-
-# puts nutritional_data = client.food_database.nutritional_data("1 large apple")
