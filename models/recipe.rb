5ed0277adf3df5632a51c7ae3c6c2807c4a34cd5
ricardop2476 <ricardop2476@hstat.org>
2018-03-08 16:56:00 +0000

    comment out model

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,5 +1,5 @@
-require 'edamam-ruby'
+# require 'edamam-ruby'
 
-client = Edamam::Client.new(app_id: "b5ae6e16", app_key: "99df03635880bb97c81ddf53c476f91a")
+# client = Edamam::Client.new(app_id: "b5ae6e16", app_key: "99df03635880bb97c81ddf53c476f91a")
 
-puts nutritional_data = client.food_database.nutritional_data("1 large apple")
+# puts nutritional_data = client.food_database.nutritional_data("1 large apple")
