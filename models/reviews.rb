83d18078b6e6b3f27e6a3518118efb14bfb9e3ff
davidx4697 <davidx4697@hstat.org>
2018-03-12 16:07:06 +0000

    fixed review api spam

---
+++ models/reviews.rb
@@ -0,0 +1,14 @@
+ #require 'net/http'
+ #require 'json'
+ #require 'pp'
+
+
+url = 'https://thereportoftheweek-api.herokuapp.com/reports?category=Energy%20Crisis'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+pp JSON.parse(response)\
+
+class Review
+    
+end
+
