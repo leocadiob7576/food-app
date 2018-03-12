f15d623c95910d2979309bdd6644fdb5499ffd8c
leocadiob7576 <leocadiob7576@hstat.org>
2018-03-12 15:48:50 +0000

    fix

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,5 +1,5 @@
-# require 'edamam-ruby'
+require 'food2fork'
 
-# client = Edamam::Client.new(app_id: "b5ae6e16", app_key: "99df03635880bb97c81ddf53c476f91a")
-
-# puts nutritional_data = client.food_database.nutritional_data("1 large apple")
+def recipe_finder
+    Recipe.search(@user_food)
+end
