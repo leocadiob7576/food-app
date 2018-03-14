7e959a2a3caa8fdcded53de72988bdcb57ab04ce
ricardop2476 <ricardop2476@hstat.org>
2018-03-14 01:39:12 +0000

    json

--- application_controller.rb
+++ application_controller.rb
@@ -16,8 +16,9 @@ class ApplicationController < Sinatra::Base
   
   
   post '/resultRecipe' do
+    user_food = params[:search]
+    @search = SearchRecipe.new(user_food)
     
-    @user_food = params[:search]
     
     erb :resultRecipe
   end  
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,5 +1,29 @@
 require 'food2fork'
+require 'net/http'
+require 'json'
+require 'pp'
 
-# ENV[FOOD2FORK_API_KEY]
+key = ENV["FOOD2FORK_API_KEY"]
 
-Food2Fork::Recipe.search
+food = @user_food
+
+class SearchRecipe
+    attr_reader :search_recipe
+    
+    def initialize(food)
+        @search_recipe = search_recipe(food)
+    end
+    
+    def search_recipe(user_food)
+        base_search = 'http://food2fork.com/api/search?key='
+        url = base_search + key + '&q=' + user_food
+        
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        pp JSON.parse(response)
+        
+        food_data["recipes"].each do
+            puts []
+        end
+    end
+end
